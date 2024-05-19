da59e9e7cc845d55a34f385b06b643e33938c952
ColdTbrew <shchoi8687@hotmail.com>
2024-05-19 12:17:29 +0900

    update 실행 코드

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 pip install -r requirements.txt
-uvicorn server.main:app --reload --host=0.0.0.0 --port=10010
+nohup uvicorn server.main:app --reload --host=0.0.0.0 --port=10010 > server.log 2>&1 &
